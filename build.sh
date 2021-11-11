a1ee9474daab9ed2c329b924d3295b239317996f
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 10:58:59 -0800

    Simplify

--- build.sh
+++ build.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-docker build --tag ghcr.io/golden-containers/node-bullseye-slim:17 working/docker-node/17/bullseye-slim
+set -xe
+rm -rf working
+mkdir working
+cd working
+
+# Checkout upstream
+
+git clone --depth 1 --branch main https://github.com/nodejs/docker-node.git
+cd docker-node
+
+# TODO: Enumerate Node versions in repo to be transformed/built/pushed
+
+# Transform
+
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" 17/bullseye/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye-slim/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye-slim/" 17/bullseye-slim/Dockerfile
+
+# Build
+
+docker build --tag ghcr.io/golden-containers/node-bullseye-slim:17 17/bullseye-slim
+
+# Push
+
+docker push ghcr.io/golden-containers/node-bullseye-slim:17
--- push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker push ghcr.io/golden-containers/node-bullseye-slim:17
