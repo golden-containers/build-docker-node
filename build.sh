4c4ec5f7b5eb689908de4fa55df0562359581815
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 12:57:29 -0800

    Refactor

--- build.sh
+++ build.sh
@@ -17,15 +17,27 @@ cd docker-node
 # This sed syntax is GNU sed specific
 [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
 
-${GNU_SED}  -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/debian\:bullseye/" 17/bullseye/Dockerfile
+${GNU_SED}  -i \
+    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" \
+    -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" \
+    17/bullseye/Dockerfile
 
-${GNU_SED} -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/" 17/bullseye-slim/Dockerfile
+${GNU_SED} -i \
+    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/; t" \
+    -e "1,// s//FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/" \
+    17/bullseye-slim/Dockerfile
 
 # Build
 
-docker build 17/bullseye/ --platform linux/amd64 --tag ghcr.io/golden-containers/node:17-bullseye --label ${1:-DEBUG=TRUE}
-docker build 17/bullseye-slim/ --platform linux/amd64 --tag ghcr.io/golden-containers/node:17-bullseye-slim --label ${1:-DEBUG=TRUE}
+[ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
+
+BUILD_PLATFORM=" --platform linux/amd64 "
+GCI_URL="ghcr.io/golden-containers"
+BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
+
+docker build 17/bullseye/ --tag ${GCI_URL}/node:17-bullseye ${BUILD_ARGS}
+docker build 17/bullseye-slim/ --tag ${GCI_URL}/node:17-bullseye-slim ${BUILD_ARGS}
 
 # Push
 
-docker push ghcr.io/golden-containers/node -a
+docker push ${GCI_URL}/node -a
